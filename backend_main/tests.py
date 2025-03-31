8fd9e5c8039d9d0207dd2fbcd1ab5250d0bb57fb
Mike Paluchowsky <miki.p102@gmail.com>
2025-03-31 12:32:36 -0400

    improved testing

--- backend_main/tests.py
+++ backend_main/tests.py
@@ -8,7 +8,7 @@ import pathlib
 templates_dir = pathlib.Path(__file__).resolve().parent / "templates" / "backend_main"
 partials_dir = pathlib.Path(__file__).resolve().parent / "templates" / "partials"
 
-
+########## Auth ##########
 class LoginViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -64,7 +64,7 @@ class LoginAPITest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.index_url)
 
-    def test_logging_in(self):
+    def test_logging_in_sucess(self):
         """Valid credentials should log in the user, set cookies, and redirect to index."""
         response = self.client.post(
             self.sing_in_api_url, {"username": "testuser", "password": "testpass"}
@@ -73,8 +73,9 @@ class LoginAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("access_token", response.cookies)
         self.assertEqual(response.cookies["access_token"].value != "", True)
+        self.assertRedirects(response, self.index_url)
 
-    def test_invalid_credentials(self):
+    def test_logging_in_fail(self):
         """Invalid credentials should return a 401 Unauthorized status."""
         response = self.client.post(
             self.sing_in_api_url, {"username": "testuser", "password": "wrongpass"}
@@ -127,7 +128,7 @@ class RegisterAPITest(TestCase):
         self.index_url = reverse("index")
 
     def test_redirect_for_logged_user(self):
-        """Test that logged-in users are redirected to the index page."""
+        """If user is logged in, they should be redirected to the index page."""
 
         user = get_user_model().objects.create_user(
             username="testuser", password="testpass"
@@ -146,7 +147,7 @@ class RegisterAPITest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.index_url)
 
-    def test_sign_up(self):
+    def test_sign_up_sucess(self):
         """Test that a valid sign-up request returns a 201 status code."""
         valid_data = {
             "username": "newuser",
@@ -161,7 +162,7 @@ class RegisterAPITest(TestCase):
         self.assertIsNotNone(user)
         self.assertEqual(user.email, "newuser@example.com")
 
-    def test_invalid_data(self):
+    def test_sign_up_fail(self):
         """Test that invalid data returns a 400 Bad Request status code."""
         invalid_data = {
             "username": "newuser",
@@ -174,7 +175,7 @@ class RegisterAPITest(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn("email", response.json())
 
-
+########## Process browser ##########
 class ProcessBrowserViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -190,16 +191,22 @@ class ProcessBrowserViewTest(TestCase):
         self.client = Client()
 
     def test_acess_denied(self):
+        """If user is not logged in, they should  be redirected to the the login page."""
         response = self.client.get(self.proc_browser_view)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.sing_in_url)
 
     def test_has_acess(self):
+        """If user logged in, they should see process browser page."""
         self.client.cookies["access_token"] = self.token
         response = self.client.get(self.proc_browser_view)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, self.proc_browser_template)
+    
+    #To fix
+    def test_partial_render_table(self):
+        pass
 
 
 class ProcessBrowserKillAPITest(TestCase):
@@ -216,6 +223,7 @@ class ProcessBrowserKillAPITest(TestCase):
         self.client = Client()
 
     def test_acess_denied(self):
+        """If user is not logged in, they should  be redirected to the the login page."""
         valid_data = {
             "KillLog_Author": self.user,
             "KillLog_Process_Name": "proc_name",
@@ -226,6 +234,18 @@ class ProcessBrowserKillAPITest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.sing_in_url)
+
+
+    #TO FIX
+    def test_kill_process_sucess(self):
+        #TODO: test killing process with sucess
+        pass
+    
+    def test_kill_process_fail(self):
+        #TODO: test killing process failed due to worng PID
+        pass
+
+
 
 
 class ProcessBrowserSnapAPITest(TestCase):
@@ -242,6 +262,7 @@ class ProcessBrowserSnapAPITest(TestCase):
         self.client = Client()
 
     def test_acess_denied(self):
+        """If user is not logged in, they should  be redirected to the the login page."""
         valid_data = {
             "snapshot_author": self.user,
         }
@@ -251,7 +272,17 @@ class ProcessBrowserSnapAPITest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.sing_in_url)
 
+    #To fix
+    def test_take_snapshot_sucess(self):
+        #TODO: create snapshot with sucess 
+        pass
+    
+    def test_take_snapshot_fail(self):
+        #TODO: create snapshot fail 
+        pass
 
+
+########## Snapshot browser ##########
 class SnapshotBrowserViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -262,57 +293,6 @@ class SnapshotBrowserViewTest(TestCase):
         cls.snapshots_url = reverse("snapshots")
         cls.sing_in_url = reverse("sign_in")
         cls.snapshots_template = f"{templates_dir}/snapshots.html"
-
-    def setUp(self):
-        self.client = Client()
-
-    def test_acess_denied(self):
-        response = self.client.get(self.snapshots_url)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, self.sing_in_url)
-
-    def test_has_access(self):
-        self.client.cookies["access_token"] = self.token
-        response = self.client.get(self.snapshots_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, self.snapshots_template)
-
-    # Need to fix this !!!
-    def Atest_deleted_snapshot(self):
-        self.test_snap_object = SnapshotObject.objects.create(
-            snapshot_author=self.user,
-        )
-
-        self.assertTrue(
-            SnapshotObject.objects.filter(
-                snapshot_id=self.test_snap_object.snapshot_id
-            ).exists()
-        )
-
-        self.client.cookies["access_token"] = self.token
-        valid_data = {
-            "snapshot_id": int(self.test_snap_object.snapshot_id),
-        }
-        response = self.client.delete(self.snapshots_url, valid_data)
-        print(response.json())
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(
-            SnapshotObject.objects.filter(
-                snapshot_id=self.test_snap_object.snapshot_id
-            ).exists()
-        )
-        # self.assertTemplateUsed(response, self.snap_details_template)
-
-
-class SnapshotDetailsViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = get_user_model().objects.create_user(
-            username="testuser", password="testpass"
-        )
-        cls.token = str(AccessToken.for_user(cls.user))
-        cls.snapshots_url = reverse("snapshots")
-        cls.sing_in_url = reverse("sign_in")
         cls.snap_details_template = f"{templates_dir}/snap_details.html"
 
     def setUp(self):
@@ -328,16 +308,155 @@ class SnapshotDetailsViewTest(TestCase):
         )
 
     def test_acess_denied(self):
+        """If user is not logged in, they should  be redirected to the the login page."""
+        response = self.client.get(self.snapshots_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.sing_in_url)
+
+    def test_has_access(self):
+        """If user logged in, they should see snaphosts browser page."""
+        self.client.cookies["access_token"] = self.token
+        response = self.client.get(self.snapshots_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.snapshots_template)
+
+    def test_details_acess_denied(self):
+        """If user is not logged in, they should  be redirected to the the login page."""
         response = self.client.get(
             f"{self.snapshots_url}?snap_id={self.test_snap_object.snapshot_id}"
         )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, self.sing_in_url)
 
-    def test_has_access(self):
+    def test_details_correct_id(self):
+        """If user logged in, they should see snaphosts details page."""
         self.client.cookies["access_token"] = self.token
         response = self.client.get(
             f"{self.snapshots_url}?snap_id={self.test_snap_object.snapshot_id}"
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, self.snap_details_template)
+
+
+
+    #To veirfy
+    def test_details_wrong_id(self):
+        self.client.cookies["access_token"] = self.token
+        response = self.client.get(
+            f"{self.snapshots_url}?snap_id={2137}"
+        )
+        self.assertEqual(response.status_code, 400)
+        
+
+    # Need to fix this !!!
+    def test_deleted_snapshot_sucess(self):
+        pass
+        #self.client.cookies["access_token"] = self.token
+        #valid_data = {
+        #    "snapshot_id": int(self.test_snap_object.snapshot_id),
+        #}
+        #response = self.client.delete(self.snapshots_url, valid_data)
+        
+        #self.assertEqual(response.status_code, 200)
+        #self.assertFalse(
+        #    SnapshotObject.objects.filter(
+        #        snapshot_id=self.test_snap_object.snapshot_id
+        #    ).exists()
+        #)
+        # self.assertTemplateUsed(response, self.snap_details_template)
+        
+    def test_deleted_snapshot_fail(self):
+        pass
+
+
+################ DRAFTS ################ 
+class SnapshotExportAPITest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+        cls.token = str(AccessToken.for_user(cls.user))
+        cls.snapshots_export_url = reverse("export_snap")
+        cls.sing_in_url = reverse("sign_in")
+        
+
+    def setUp(self):
+        self.client = Client()
+        self.test_snap_object = SnapshotObject.objects.create(
+            snapshot_author=self.user,
+        )
+
+        self.assertTrue(
+            SnapshotObject.objects.filter(
+                snapshot_id=self.test_snap_object.snapshot_id
+            ).exists()
+        )
+
+    def test_acess_denied(self):
+        valid_data = {"snap_id":self.test_snap_object.snapshot_id}
+        response = self.client.get(snapshots_export_url,valid_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.sing_in_url)
+
+
+    #To fix
+    def test_export_snapshot_sucess(self):
+        self.client.cookies["access_token"] = self.token
+        valid_data = {"snap_id":self.test_snap_object.snapshot_id}
+
+        response = self.client.get(snapshots_export_url,valid_data)
+
+        self.assertEqual(response.status_code, 200)
+        #TODO: 
+        # - check file
+        # - add test processes
+       # self.assertTemplateUsed(response, self.snap_details_template)
+
+    #To fix
+    def test_export_snapshot_fail(self):
+        pass
+
+
+class KillLogBrowserViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+        cls.token = str(AccessToken.for_user(cls.user))
+        cls.kill_log_url = reverse("kill-log")
+        cls.sing_in_url = reverse("sign_in")
+        cls.kill_log_template = f"{templates_dir}/kill-log.html"
+
+    def setUp(self):
+        self.client = Client()
+        self.test_snap_object = SnapshotObject.objects.create(
+            snapshot_author=self.user,
+        )
+
+        self.assertTrue(
+            SnapshotObject.objects.filter(
+                snapshot_id=self.test_snap_object.snapshot_id
+            ).exists()
+        )
+
+    def test_acess_denied(self):
+        response = self.client.get(kill_log_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.sing_in_url)
+
+    def test_has_access(self):
+        self.client.cookies["access_token"] = self.token        
+        response = self.client.get(kill_log_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.kill_log_template)
+    
+
+    #To fix
+    def test_remove_kill_log_entry_sucess(self):
+        pass
+
+    def test_remove_kill_log_entry_fail(self):
+        pass
