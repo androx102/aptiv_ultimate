f87da1e9645c5bc46e18ea46beddb3e3e044b0fd
androx102 <androx102@users.noreply.github.com>
2025-04-01 15:14:03 +0000

    style fixes by ruff

--- backend_main/tests.py
+++ backend_main/tests.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 import pathlib
 import unittest
-import json
 from rest_framework_simplejwt.tokens import AccessToken
 from .models import *
 
@@ -479,7 +478,12 @@ class KillLogBrowserViewTest(TestCase):
             "kill_id": self.test_killlog_object.KillLog_ID,
         }
 
-        response = self.client.delete(self.kill_log_url, valid_data, format="json",content_type='application/json')
+        response = self.client.delete(
+            self.kill_log_url,
+            valid_data,
+            format="json",
+            content_type="application/json",
+        )
 
         self.assertEqual(response.status_code, 200)
 
